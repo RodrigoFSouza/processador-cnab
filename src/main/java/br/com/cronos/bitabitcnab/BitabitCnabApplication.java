2e4659f31ff714cecfb3a52c06c968a5edb8141b
rodrigo <rodrigo.souza@goharry.com.br>
2024-02-01 18:59:40 -0300

    começando a criar o batch para o processar o cnab em background

---
+++ src/main/java/br/com/cronos/bitabitcnab/BitabitCnabApplication.java
@@ -0,0 +1,13 @@
+package br.com.cronos.bitabitcnab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitabitCnabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BitabitCnabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/cronos/bitabitcnab/BitabitCnabApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cronos.bitabitcnab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitabitCnabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
